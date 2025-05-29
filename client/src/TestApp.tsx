f891d26c7b0ad97abb72b96e9d1d99a3da7db0d9
eyg6062 <anele9999@gmail.com>
2025-05-28 20:11:09 -0400

    added react router

--- client/src/TestApp.tsx
+++ client/src/TestApp.tsx
@@ -1,14 +1,24 @@
 import './index.css'
 import reactLogo from './assets/react.svg'
+import Header from './components/Header'
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
 
 function TestApp() {
     return(
-        <>
-            <main>
-                <img src={reactLogo} width="40px"/>
-                <h1>meep</h1>
-            </main>
-        </>
+        <main>
+            <img src={reactLogo} width="40px"/>
+            <h1>meep</h1>
+            
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </main>
     )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-//import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
+
+import Home from './pages/Home.tsx'
+import Dashboard from './pages/Dashboard.tsx'
+import About from './pages/About.tsx'
 import TestApp from './TestApp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TestApp />
+    <BrowserRouter>
+      <TestApp />
+    </BrowserRouter>
   </StrictMode>,
 )
